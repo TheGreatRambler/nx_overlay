4643dcbe2daad16dafbe07b0990779fec3bc7491
TheGreatRambler <aehart684351@gmail.com>
2020-01-12 05:40:38 +0000

    Added starting IPC server code
    Signed-off-by: TheGreatRambler <aehart684351@gmail.com>

---
+++ source/ispService.cpp
@@ -0,0 +1,130 @@
+#include "ispService.hpp"
+
+IpsService::IpsService() {
+	// First, set up getting framebuffer code
+	// Get the PID from the Title ID
+	pmdmntGetProcessId(&VI_pid, VITitleId);
+	// Get the pointer to the main framebuffer in VI
+	Result rc = svcDebugActiveProcess(&VIdbg, VI_pid);
+	if(R_SUCCEEDED(rc)) {
+		uint64_t addr = 0;
+		// Loops for 0x1000 because it shouldn't go infinitely
+		for(int i = 0; i < 0x1000; i++) {
+			MemoryInfo info;
+			uint32_t pageinfo;
+			rc = svcQueryDebugProcessMemory(&info, &pageinfo, VIdbg, addr);
+			if(info.type == MemType_CodeMutable) {
+				if(info.size == 0x1A40000) {
+					// Set the address now that it is known
+					framebufferPointer = info.addr;
+					break;
+				}
+			} else if(info.type == MemType_Reserved) {
+				break;
+			}
+			addr += info.size;
+		}
+		svcCloseHandle(VIdbg);
+	}
+	// Next, set up the overlay code
+	// https://github.com/averne/dvdnx/blob/master/src/screen.cpp
+	rc = smInitialize();
+	if(R_FAILED(rc)) {
+		fatalThrow(rc);
+	}
+	// ViServiceType_Manager defined here https://switchbrew.github.io/libnx/vi_8h.html
+	rc = viInitialize(ViServiceType_Manager);
+	if(R_FAILED(rc)) {
+		fatalThrow(rc);
+	}
+	rc = viOpenDefaultDisplay(&display);
+	if(R_FAILED(rc)) {
+		viExit();
+		return;
+	}
+	// flag 0 allows non-fullscreen layer
+	rc = viCreateManagedLayer(&display, (ViLayerFlags)0, 0, &__nx_vi_layer_id);
+	if(R_FAILED(rc)) {
+		viCloseDisplay(&display);
+		fatalThrow(rc);
+	}
+	rc = viCreateLayer(&display, &layer);
+	if(R_FAILED(rc)) {
+		viDestroyManagedLayer(&layer);
+		fatalThrow(rc);
+	}
+	rc = viSetLayerScalingMode(&layer, ViScalingMode_FitToLayer);
+	if(R_FAILED(rc)) {
+		viDestroyManagedLayer(&layer);
+		fatalThrow(rc);
+	}
+	// Arbitrary z index
+	rc = viSetLayerZ(&layer, 100);
+	if(R_FAILED(rc)) {
+		viDestroyManagedLayer(&layer);
+		fatalThrow(rc);
+	}
+	// These might not be screenWidth and screenHeight TODO
+	// They are smaller in the source
+	rc = viSetLayerSize(&layer, screenWidth, screenHeight);
+	if(R_FAILED(rc)) {
+		viDestroyManagedLayer(&layer);
+		fatalThrow(rc);
+	}
+	// The X and Y positions of the layer
+	rc = viSetLayerPosition(&layer, 0.0f, 0.0f);
+	if(R_FAILED(rc)) {
+		viDestroyManagedLayer(&layer);
+		fatalThrow(rc);
+	}
+	rc = nwindowCreateFromLayer(&window, &layer);
+	if(R_FAILED(rc)) {
+		viDestroyManagedLayer(&layer);
+		fatalThrow(rc);
+	}
+	// PIXEL_FORMAT_RGBA_8888  defined in LibNX (based on Android)
+	// Not PIXEL_FORMAT_RGBA_8888 in source, but I don't care
+	// Upscaling and downscaling will happen so that the Layer Size and the FB size match
+	rc = framebufferCreate(&framebuf, &window, screenWidth, screenHeight, PIXEL_FORMAT_RGBA_8888, 1);
+	if(R_FAILED(rc)) {
+		nwindowClose(&window);
+		fatalThrow(rc);
+	}
+
+	// Make Framebuffer linear to make things easier
+	// Imma too dumb to figure out the raw format
+	// 4 bytes per pixel (outstride)
+	// rc = framebufferMakeLinear(&framebuf);
+	// if(R_FAILED(rc)) {
+	//	nwindowClose(&window);
+	//	fatalThrow(rc);
+	//}
+	// Not actually making the framebuffer linear to save memory
+}
+
+ams::Result IspService::StartGetFramebuffer(s16 width, s16 height, s16 x, s16 y) {
+	// Check this https://github.com/TheGreatRambler/nx-TAS/blob/dev/source/handle_savestates.cpp
+}
+
+ams::Result IspService::GetFramebufferChunk(s32 bytesToRead) { }
+
+ams::Result IspService::EndGetFramebuffer() { }
+
+ams::Result IspService::SetupOverlay(s8 layer) { }
+
+ams::Result IspService::StartWriteOverlay(s16 width, s16 height, s16 x, s16 y) {
+	// Check this https://github.com/TheGreatRambler/NX-TAS-UI/blob/dev/sysmodule/source/writeToScreen.hpp
+}
+
+ams::Result IspService::WriteOverlayChunk(s32 bytesToWrite) { }
+
+ams::Result IspService::EndWriteOverlay() { }
+
+~IpsService::IpsService() {
+	// Close everything
+	framebufferClose(&framebuf);
+	nwindowClose(&window);
+	viDestroyManagedLayer(&layer);
+	viCloseDisplay(&display);
+	viExit();
+}
---
+++ source/ispService.hpp
@@ -0,0 +1,177 @@
+#pragma once
+
+#include <stratosphere/fssrv/fssrv_interface_adapters.hpp>
+
+// I dunno what this is
+extern "C" u64 __nx_vi_layer_id;
+
+class IpsService : public ams::sf::IServiceObject {
+
+private:
+	enum CommandId {
+		// A short description of each
+		// This allows you to set the width, height, x, and y of the to-be-read framebuffer
+		// It also sets the framebuffer code to read
+		StartGetFramebuffer = 0,
+		// Get a certain number of bytes of the specified framebuffer
+		GetFramebufferChunk = 1,
+		// End the framebuffer reading right now
+		EndGetFramebuffer = 2,
+		// This can only be run once, it sets things such as the layer and otherwise
+		SetupOverlay = 3,
+		// This allows you to set the width, height, x, and y of the overlay right now
+		StartWriteOverlay = 4,
+		// Write a specific number of bytes
+		WriteOverlayChunk = 5,
+		// Finish it off
+		EndWriteOverlay = 6,
+	};
+
+	// Error handling for everything
+	Result rc;
+
+	// Framebuffer stuff
+	u64 VI_pid;
+	// The Title Id of VI (The display manager)
+	static constexpr u64 VITitleId = 0x010000000000002D;
+	Handle VIdbg;
+	// Size of the framebuffer
+	// Width * Height * Byte depth (1280 * 720 * 4)
+	static constexpr u64 framebufferSize = 3686400;
+
+	// Pointer to framebuffer data in VI
+	u64 framebufferPointer;
+
+	// Overlay stuff
+	ViDisplay display;
+	ViLayer layer;
+	NWindow window;
+	Framebuffer framebuf;
+	// Current pointer to the graphics data
+	u8* currentBuffer;
+
+public:
+	IpsService();
+
+	ams::Result StartGetFramebuffer(s16 width, s16 height, s16 x, s16 y);
+
+	ams::Result GetFramebufferChunk(s32 bytesToRead);
+
+	ams::Result EndGetFramebuffer();
+
+	// Doesn't actually create a new window, just used for internal logic
+	ams::Result SetupOverlay(s8 layer);
+
+	ams::Result StartWriteOverlay(s16 width, s16 height, s16 x, s16 y);
+
+	ams::Result WriteOverlayChunk(s32 bytesToWrite);
+
+	ams::Result EndWriteOverlay();
+
+	~IpsService();
+
+	/*
+		void ListMountedDrives(const ams::sf::OutArray<s32>& out_interface_ids, ams::sf::Out<s32> out_count) {
+			FSP_USB_LOG("%s: forcing a mounted drive list update.", __func__);
+			impl::DoUpdateDrives();
+
+			size_t drive_count     = impl::GetAcquiredDriveCount();
+			size_t buf_drive_count = std::min(drive_count, out_interface_ids.GetSize());
+			FSP_USB_LOG("%s: drive count -> %lu | output drive count -> %lu.", __func__, drive_count, buf_drive_count);
+
+			for(u32 i = 0; i < buf_drive_count; i++) {
+				out_interface_ids[i] = impl::GetDriveInterfaceId(i);
+				FSP_USB_LOG("%s: interface ID #%u -> %d.", __func__, i, out_interface_ids[i]);
+			}
+
+			out_count.SetValue(static_cast<s32>(buf_drive_count));
+		}
+
+		ams::Result GetDriveFileSystemType(s32 drive_interface_id, ams::sf::Out<u8> out_fs_type) {
+			FSP_USB_LOG("%s (interface ID %d): forcing a mounted drive list update.", __func__, drive_interface_id);
+			impl::DoUpdateDrives();
+			R_UNLESS(impl::IsDriveInterfaceIdValid(drive_interface_id), ResultInvalidDriveInterfaceId());
+
+			impl::DoWithDriveFATFS(drive_interface_id, [&](FATFS* fs) {
+				out_fs_type.SetValue(fs->fs_type);
+				FSP_USB_LOG("%s (interface ID %d): set filesystem type to 0x%02X.", __func__, drive_interface_id, fs->fs_type);
+			});
+
+			return ams::ResultSuccess();
+		}
+
+		ams::Result GetDriveLabel(s32 drive_interface_id, ams::sf::OutBuffer& out_label_str) {
+			FSP_USB_LOG("%s (interface ID %d): forcing a mounted drive list update.", __func__, drive_interface_id);
+			impl::DoUpdateDrives();
+			R_UNLESS(impl::IsDriveInterfaceIdValid(drive_interface_id), ResultInvalidDriveInterfaceId());
+
+			auto drive_mounted_idx = impl::GetDriveMountedIndex(drive_interface_id);
+			FSP_USB_LOG("%s (interface ID %d): drive index -> %u.", __func__, drive_interface_id, drive_mounted_idx);
+
+			char mountname[0x10] = { 0 };
+			impl::FormatDriveMountName(mountname, drive_mounted_idx);
+			FSP_USB_LOG("%s (interface ID %d): drive mount name -> \"%s\".", __func__, drive_interface_id, mountname);
+
+			auto ffrc = f_getlabel(mountname, reinterpret_cast<char*>(out_label_str.GetPointer()), nullptr);
+			FSP_USB_LOG("%s (interface ID %d): f_getlabel returned %u.", __func__, drive_interface_id, ffrc);
+
+			return result::CreateFromFRESULT(ffrc);
+		}
+
+		ams::Result SetDriveLabel(s32 drive_interface_id, ams::sf::InBuffer& label_str) {
+			FSP_USB_LOG("%s (interface ID %d): forcing a mounted drive list update.", __func__, drive_interface_id);
+			impl::DoUpdateDrives();
+			R_UNLESS(impl::IsDriveInterfaceIdValid(drive_interface_id), ResultInvalidDriveInterfaceId());
+
+			auto drive_mounted_idx = impl::GetDriveMountedIndex(drive_interface_id);
+			FSP_USB_LOG("%s (interface ID %d): drive index -> %u.", __func__, drive_interface_id, drive_mounted_idx);
+
+			char mountname[0x10] = { 0 };
+			impl::FormatDriveMountName(mountname, drive_mounted_idx);
+			FSP_USB_LOG("%s (interface ID %d): drive mount name -> \"%s\".", __func__, drive_interface_id, mountname);
+
+			// Check that no more than 11 characters are copied
+			// Also allow an empty label_str (label removal)
+			char newname[0x100]     = { 0 };
+			const char* input_label = reinterpret_cast<const char*>(label_str.GetPointer());
+
+			if(strlen(input_label) > 0) {
+				char label[0x10] = { 0 };
+				snprintf(label, 11, input_label);
+				sprintf(newname, "%s%s", mountname, label);
+			} else {
+				sprintf(newname, mountname);
+			}
+
+			FSP_USB_LOG("%s (interface ID %d): drive label -> \"%s\".", __func__, drive_interface_id, newname);
+
+			auto ffrc = f_setlabel(newname);
+			FSP_USB_LOG("%s (interface ID %d): f_setlabel returned %u.", __func__, drive_interface_id, ffrc);
+
+			return result::CreateFromFRESULT(ffrc);
+		}
+
+		ams::Result OpenDriveFileSystem(s32 drive_interface_id, ams::sf::Out<std::shared_ptr<IFileSystemInterface>> out_fs) {
+			FSP_USB_LOG("%s (interface ID %d): forcing a mounted drive list update.", __func__, drive_interface_id);
+			impl::DoUpdateDrives();
+			R_UNLESS(impl::IsDriveInterfaceIdValid(drive_interface_id), ResultInvalidDriveInterfaceId());
+
+			std::shared_ptr<ams::fs::fsa::IFileSystem> drv_fs = std::make_shared<DriveFileSystem>(drive_interface_id);
+			out_fs.SetValue(std::make_shared<IFileSystemInterface>(std::move(drv_fs), false));
+			FSP_USB_LOG("%s (interface ID %d): IFileSystem object created.", __func__, drive_interface_id);
+
+			return ams::ResultSuccess();
+		}
+		*/
+
+	// Corresponds to each CommandId
+	DEFINE_SERVICE_DISPATCH_TABLE {
+		MAKE_SERVICE_COMMAND_META(StartGetFramebuffer),
+		MAKE_SERVICE_COMMAND_META(GetFramebufferChunk),
+		MAKE_SERVICE_COMMAND_META(EndGetFramebuffer),
+		MAKE_SERVICE_COMMAND_META(SetupOverlay),
+		MAKE_SERVICE_COMMAND_META(StartWriteOverlay),
+		MAKE_SERVICE_COMMAND_META(WriteOverlayChunk),
+		MAKE_SERVICE_COMMAND_META(EndWriteOverlay),
+	};
+};
